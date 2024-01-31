d50020f00783855abdc6ec47c771d9b1f554232c
MEEYAD\anhttm <tranmaianhnd@gmail.com>
2024-01-31 16:05:51 +0700

    update checkout page

--- assets/js/customs.js
+++ assets/js/customs.js
@@ -8,7 +8,7 @@ jQuery(document).ready(function ($) {
     $('#sidebar-menu').removeClass('show');
     $('#mobile-menu-overlay').removeClass('show');
   });
-  $('.check-list-quantity li').on('click', function () {
+  $('.check-variable-item').on('click', function () {
     $(this).toggleClass('selected');
   });
   $('.go-show-mini-cart').on('click', function () {
@@ -17,20 +17,23 @@ jQuery(document).ready(function ($) {
   $('.cart-sidebar-header button.close').on('click', function () {
     $('#cart-sidebar').removeClass('show');
   });
+  $('.order-summary-header').click(function () {
+    $('.order-summary').toggleClass('show');
+  });
   $(document).on('scroll', function () {
     if ($('#description-content').length > 0) {
-        let offsetTop = $('#description-content').offset().top;
-        if ($(this).scrollTop() >= offsetTop) {
-            $('#buy-now-btm').fadeIn();
-        } else
-            $('#buy-now-btm').fadeOut();
+      let offsetTop = $('#description-content').offset().top;
+      if ($(this).scrollTop() >= offsetTop) {
+        $('#buy-now-btm').fadeIn();
+      } else
+        $('#buy-now-btm').fadeOut();
     }
-});
+  });
   $('#buy-now-btm').on('click', function () {
     $('html,body').animate({
-        scrollTop: $('#sales-countdown').offset().top + 10
+      scrollTop: $('#sales-countdown').offset().top + 10
     }, 500)
-});
+  });
   if ($('.slider-for').length > 0) {
     $('.slider-for').slick({
       slidesToShow: 1,
@@ -75,6 +78,25 @@ jQuery(document).ready(function ($) {
       $('#sales-countdown span').attr('data-time', distance - 1)
     }, 1000);
   }
+  if ($('#checkout-coutndown').length > 0) {
+    // Update the count down every 1 second
+    let x = setInterval(function () {
+      // Find the distance between now and the count down date
+      let distance = parseInt($('#checkout-countdown-time').attr('data-time'));
+
+      // Time calculations for days, hours, minutes and seconds
+      let minutes = Math.floor(distance / 60);
+      let seconds = Math.floor(distance % 60);
+      $('#checkout-countdown-time').html(`${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`)
+      // If the count down is finished, write some text
+      if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("checkout-countdown-time").innerHTML = "EXPIRED";
+      }
+
+      $('#checkout-countdown-time').attr('data-time', distance - 1)
+    }, 1000);
+  }
   (function quantityProducts() {
     var $quantityArrowMinus = $(".quantity-arrow-minus");
     var $quantityArrowPlus = $(".quantity-arrow-plus");
@@ -93,7 +115,7 @@ jQuery(document).ready(function ($) {
       $quantityNum.val(+$quantityNum.val() + 1);
     }
   })();
-  
+
 });
 
 
